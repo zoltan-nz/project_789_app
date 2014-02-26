97038d669d58635b17775b9a0045e9d9a22b2702
Zoltan <zdebre@gmail.com>
2014-02-26 00:40:29 +0000

    active admin installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users
+  ActiveAdmin.routes(self)
   root 'posts#index'
   resources :posts
 
--- db/migrate/20140226003248_devise_create_admin_users.rb
+++ db/migrate/20140226003248_devise_create_admin_users.rb
@@ -1,12 +1,12 @@
-class DeviseCreateUsers < ActiveRecord::Migration
+class DeviseCreateAdminUsers < ActiveRecord::Migration
   def migrate(direction)
     super
     # Create a default user
-    User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if direction == :up
+    AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if direction == :up
   end
 
   def change
-    create_table(:users) do |t|
+    create_table(:admin_users) do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -40,9 +40,9 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    add_index :admin_users, :email,                unique: true
+    add_index :admin_users, :reset_password_token, unique: true
+    # add_index :admin_users, :confirmation_token,   unique: true
+    # add_index :admin_users, :unlock_token,         unique: true
   end
 end
