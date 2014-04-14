973169f84535624b33497cb5ca6b022f4ec71310
Zoltan <zdebre@gmail.com>
2014-04-14 22:56:24 +1200

    admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
 
   resources :posts
 
+  namespace :admin do 
+    get '/',    to: 'admin#index'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
