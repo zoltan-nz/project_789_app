aae76cb15321320632b541fa40aef8013ea10b3d
Zoltan <zdebre@gmail.com>
2014-05-16 22:24:31 +1200

    update user

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -1,8 +1,8 @@
 App.User = DS.Model.extend({
 
-  email: DS.attr('string'),
-  admin: DS.attr('boolean'),
-  firstName: DS.attr('string'),
-  lastName: DS.attr('string')
+  email:      DS.attr('string'),
+  admin:      DS.attr('boolean'),
+  firstName:  DS.attr('string'),
+  lastName:   DS.attr('string')
 
 });
