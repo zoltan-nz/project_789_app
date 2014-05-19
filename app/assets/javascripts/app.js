8952724850dfba6546a3e29041e82c699dea80dc
Zoltan <zdebre@gmail.com>
2014-05-19 22:57:40 +1200

    fixing ruby version

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,10 +1,10 @@
 App = window.App = Em.Application.create({
-  rootElement: '#ember-app',
-  routeUrl: '/admin/',
-  LOG_TRANSITIONS_INTERNAL: true,
-  LOG_ACTIVE_GENERATION: true,
-  LOG_VIEW_LOOKUPS: true,
-  LOG_BINDINGS: true
+    rootElement: '#ember-app',
+    routeUrl: '/admin/',
+    LOG_TRANSITIONS_INTERNAL: true,
+    LOG_ACTIVE_GENERATION: true,
+    LOG_VIEW_LOOKUPS: true,
+    LOG_BINDINGS: true
 });
 
 Em.run.backburner.DEBUG = true;
